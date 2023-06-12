5d762cd6d452e56065295c881edd04f8e7a45fba
Oh-byeongju <dhqudwn0@naver.com>
2023-06-12 20:15:16 +0900

    Linux 시간 전역설정

--- Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
+++ Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
@@ -3,10 +3,19 @@ package com.movie.Spring_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MovieServerApplication {
 
-	public static void main(String[] args) throws InterruptedException {
+	// Linux 환경에서의 시간 설정
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(MovieServerApplication.class, args);
 		System.out.println("서버 작동");
 	}

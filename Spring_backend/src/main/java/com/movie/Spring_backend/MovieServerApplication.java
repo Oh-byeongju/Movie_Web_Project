8643621406def5983b43007eaf14714408221240
Oh-byeongju <dhqudwn0@naver.com>
2023-06-12 23:32:49 +0900

    Timezone 설정

--- Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
+++ Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
@@ -3,17 +3,8 @@ package com.movie.Spring_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class MovieServerApplication {
-
-	// Linux 환경에서의 시간 설정
-	@PostConstruct
-	public void started(){
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieServerApplication.class, args);
